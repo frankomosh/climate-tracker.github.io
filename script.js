c00bd812be989f71f1b60cd4f3df8c6a1fd7133d
Frank-197 <frankomosh197@gmail.com>
2022-06-24 14:53:53 +0300

    adding key event listeners

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const wrapper = document.querySelector(".wrapper"),
     arrowBack = wrapper.querySelector("header i");
 
 let api;
+inputField.addEventListener("keyup", e => {
+    //if user pressed Enter btn and input value is not empty
+    if (e.key == "Enter" && inputField.value != "") {
+        requestApi(inputField.value);
+    }
+});
+
+locationBtn.addEventListener("click", () => {
+    if (navigator.geolocation) { // if browser supports geolocation API
+        //geolocation.getCurrentPosition method is used to get the current position of the device
+        //it takes three parameters: success, error and options. If everything is right then sucess
+        //callback function will call else error callback function will call. 
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        alert("Your browser not support geolocation api");
+    }
+});
