1c9de0ca13aaf4acd59b0405a19e54da8e64a4ba
Frank-197 <frankomosh197@gmail.com>
2022-06-24 14:56:58 +0300

    adding functions

--- script.js
+++ script.js
@@ -35,3 +35,61 @@ function onSuccess(position) {
     api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=094b3c61c39ce8237f2e4b22266e472b`;
     fetchData();
 }
+function onError(error) {
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
+}
+
+
+function fetchData() {
+    infoTxt.innerText = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    //getting api response and returning it with parsing into js object and in another
+    //then function calling weatherDetails funstion with passing api result as an argument
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() => {
+        infoTxt.innerText = "Something went wrong";
+        infoTxt.classList.replace("pending", "error");
+    });
+}
+
+function weatherDetails(info) {
+    if (info.cod == "404") {
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+    } else { ///getting required properties value from info object
+        const city = info.name;
+        const country = info.sys.country;
+        const { description, id } = info.weather[0];
+        const { temp, feels_like, humidity } = info.main;
+
+        //displaying dymanic images accprding to the id returned in the weather object by the api
+        if (id == 800) {
+            wIcon.src = "icons/clear.svg";
+        } else if (id >= 200 && id <= 232) {
+            wIcon.src = "icons/storm.svg";
+        } else if (id >= 600 && id <= 622) {
+            wIcon.src = "icons/snow.svg";
+        } else if (id >= 701 && id <= 781) {
+            wIcon.src = "icons/haze.svg";
+        } else if (id >= 801 && id <= 804) {
+            wIcon.src = "icons/cloud.svg";
+        } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+            wIcon.src = "icons/rain.svg";
+        }
+
+        //passing these values to a particular html element
+        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+        weatherPart.querySelector(".weather").innerText = description;
+        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+        infoTxt.classList.remove("pending", "error");
+        infoTxt.innerText = "";
+        inputField.value = "";
+        wrapper.classList.add("active");
+    }
+}
+
+arrowBack.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+});
